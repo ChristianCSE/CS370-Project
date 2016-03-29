65dbae8e96748c0be77a2995aa7f87dda54b6355
Chris Oh <seokkyu125@gmail.com>
2016-03-29 17:54:44 -0400

    favicon

--- html/templates/header.php
+++ html/templates/header.php
@@ -3,7 +3,6 @@
 <html lang="en">
 
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -11,6 +10,9 @@
     <meta name="author" content="">
 
     <title>Roomie</title>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/ico" href="HouseIcon2.ico" />
 
     <!-- Bootstrap Core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
